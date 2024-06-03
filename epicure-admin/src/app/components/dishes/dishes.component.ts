ea0a3242874876ea5648cfb7a89afe97830217a1
romi kamin <romik@moveo.co.il>
2024-06-03 14:42:00 +0300

    admin app stanalone tested

--- epicure-admin/src/app/components/dishes/dishes.component.ts
+++ epicure-admin/src/app/components/dishes/dishes.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dishes',
-  standalone: true,
-  imports: [],
   templateUrl: './dishes.component.html',
   styleUrl: './dishes.component.scss'
 })
